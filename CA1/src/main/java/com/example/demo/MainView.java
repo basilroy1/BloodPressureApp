752d953aece14f8e2e21d0e5d86f51eaf371ba5b
Basil Roy <basil.k.roy@hotmail.com>
2022-10-06 22:05:15 +0100

    testing UI changes

--- CA1/src/main/java/com/example/demo/MainView.java
+++ CA1/src/main/java/com/example/demo/MainView.java
@@ -1,11 +1,18 @@
 package com.example.demo;
 
+import com.vaadin.addon.charts.Chart;
+import com.vaadin.addon.charts.model.ChartType;
+import com.vaadin.addon.charts.model.ListSeries;
+import com.vaadin.addon.charts.model.PlotOptionsColumn;
+import com.vaadin.addon.charts.model.Stacking;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.html.Input;
@@ -21,15 +28,22 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.w3c.dom.html.HTMLTitleElement;
 
+import java.util.Collection;
+
 @PageTitle("BloodPressureCalculator")
 @Route("")
 public class MainView extends VerticalLayout {
 
   public MainView() {
-    add(new H1("Blood Pressure Calculator"));
+
+    H1 header = new H1("Blood Pressure Calculator");
+    header.getStyle().set("margin","auto");
+    header.getStyle().set("color","orange");
+    add(header);
 
     IntegerField systolicValue = new IntegerField("Enter Systolic Value (mmHG)");
     IntegerField diastolicValue = new IntegerField("Enter Diastolic Value (mmHG)");
+
 
     systolicValue.setRequiredIndicatorVisible(true);
     systolicValue.setWidth(275,Unit.PIXELS);
@@ -42,25 +56,51 @@ public class MainView extends VerticalLayout {
 
     HorizontalLayout horizontalLayout = new HorizontalLayout(systolicValue,diastolicValue);
     horizontalLayout.setDefaultVerticalComponentAlignment(Alignment.CENTER);
-
+    horizontalLayout.getStyle().set("margin","auto");
     add(horizontalLayout);
 
     Button calculate_bp = new Button("Calculate BP");
     calculate_bp.getStyle().set("color","black");
-    calculate_bp.getStyle().set("margin-left","200px");
-    add(calculate_bp);
+    calculate_bp.getStyle().set("margin","auto");
 
+    add(calculate_bp);
 
     calculate_bp.addClickListener(click -> {
 
      String res = BusinessLogic.checkBloodPressureStatus(
          (systolicValue.getValue()),diastolicValue.getValue());
 
-      Notification.show("Hi, You have "+res,7000, Notification.Position.MIDDLE);
+      Notification.show(res,7000, Notification.Position.MIDDLE);
       //add(new Paragraph(res));
       systolicValue.setValue(null);
       diastolicValue.setValue(null);
-  });
+
+
+   /* Dialog dialog = new Dialog();
+    Button deleteButton = new Button("Delete", (e) -> dialog.close());
+
+    deleteButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY,
+        ButtonVariant.LUMO_ERROR);
+    deleteButton.getStyle().set("margin-right", "auto");
+    dialog.getFooter().add(deleteButton);
+    add(deleteButton);
+
+    Button cancelButton = new Button("Cancel", (e) -> dialog.close());
+    cancelButton.addThemeVariants(ButtonVariant.LUMO_TERTIARY);
+    dialog.getFooter().add(cancelButton);
+    add(cancelButton);*/
+    });
     calculate_bp.addClickShortcut(Key.ENTER);
+    Chart chart = new Chart(ChartType.BAR);
+    chart.setWidth("400px");  // 100% by default
+    chart.setHeight("300px"); // 400px by default
+    chart.drawChart();
+
+    ListSeries series = new ListSeries(50, 60, 70, 80);
+    PlotOptionsColumn plotOptions = new PlotOptionsColumn();
+    plotOptions.setStacking(Stacking.NORMAL);
+    series.setPlotOptions(plotOptions);
+
   }
+
 }
