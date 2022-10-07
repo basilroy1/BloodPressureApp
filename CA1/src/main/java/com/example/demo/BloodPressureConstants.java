4d145516799284df9c2c3df2b6d0295cd42f6b5a
Basil Roy <basil.k.roy@hotmail.com>
2022-10-07 17:08:04 +0100

    fixxed some of the sonar issues

--- CA1/src/main/java/com/example/demo/BloodPressureConstants.java
+++ CA1/src/main/java/com/example/demo/BloodPressureConstants.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
 public class BloodPressureConstants {
-  public static final int SystolicMin = 70;
-  public static final int SystolicMax = 190;
-  public static final int DiastolicMin = 40;
-  public static final int DiastolicMax = 100;
+  public static final int systolicMin = 70;
+  public static final int systolicMax = 190;
+  public static final int diastolicMin = 40;
+  public static final int diastolicMax = 100;
   public static String LOW = "Low Blood Pressure";
   public static String IDEAL = "Ideal Blood Pressure";
   public static String PREHIGH = "Pre-High Blood Pressure";
--- CA1/src/main/java/com/example/demo/BusinessLogic.java
+++ CA1/src/main/java/com/example/demo/BusinessLogic.java
@@ -7,10 +7,10 @@ public class BusinessLogic {
   }
 
   public static String checkSystolicAndDiastolicIsInAllowedRange(int x, int y) {
-    if (x < BloodPressureConstants.SystolicMin || x > BloodPressureConstants.SystolicMax) {
+    if (x < BloodPressureConstants.systolicMin || x > BloodPressureConstants.systolicMax) {
       return BloodPressureConstants.INVALID_SYSTOLIC;
     }
-    if (y < BloodPressureConstants.DiastolicMin || y > BloodPressureConstants.DiastolicMax) {
+    if (y < BloodPressureConstants.diastolicMin || y > BloodPressureConstants.diastolicMax) {
       return BloodPressureConstants.INVALID_DIASTOLIC;
     }
     return "Values are Perfect !";
@@ -19,13 +19,13 @@ public class BusinessLogic {
   public static String checkBloodPressureStatus(int x, int y){
     String bpStatus = "";
     if(checkSystolicisAlwaysHigherThanDiastolic(x ,y)) {
-      if ((x >= BloodPressureConstants.SystolicMin && x <= 90) && (y >= BloodPressureConstants.DiastolicMin && y <= 60)) {
+      if ((x >= BloodPressureConstants.systolicMin && x <= 90) && (y >= BloodPressureConstants.diastolicMin && y <= 60)) {
         bpStatus = BloodPressureConstants.LOW;
       } else if ((x > 90 && x <= 120) && (y > 60 && y <= 80)) {
         bpStatus = BloodPressureConstants.IDEAL;
       } else if ((x > 120 && x <= 140) && (y > 80 && y <= 90)) {
         bpStatus = BloodPressureConstants.PREHIGH;
-      } else if ((x > 140 && x <= BloodPressureConstants.SystolicMax) && (y > 90 && y <= BloodPressureConstants.DiastolicMax)) {
+      } else if ((x > 140 && x <= BloodPressureConstants.systolicMax) && (y > 90 && y <= BloodPressureConstants.diastolicMax)) {
         bpStatus = BloodPressureConstants.HIGH;
       } else {
         return checkSystolicAndDiastolicIsInAllowedRange(x, y);
