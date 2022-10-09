afe0e25ae5577bb0e428043728b0f1bde8e209b9
Basil Roy <basil.k.roy@hotmail.com>
2022-10-09 22:07:45 +0100

    addded new test class

---
+++ CA1/src/test/java/com/example/demo/MainViewTest.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+public class MainViewTest {
+}
